a0825183655e5105bea3f5ea504edc609eb62e67
Yuri <kenny21@yandex.ru>
2024-08-30 22:06:05 +0300

    Task B-5.4: Add logging, serilog and seq

---
+++ backend/src/PetHome.API/Controllers/BaseContoller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetHome.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public abstract class BaseContoller : ControllerBase
+    {
+        protected ILogger<BaseContoller> _logger;
+        public BaseContoller(ILogger<BaseContoller> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
--- backend/src/PetHome.API/Controllers/VolunteersController.cs
+++ backend/src/PetHome.API/Controllers/VolunteersController.cs
@@ -4,19 +4,25 @@ using PetHome.Application.Volunteers.CreateVolunteer;
 
 namespace PetHome.API.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class VolunteersController : ControllerBase
+   
+    public class VolunteersController : BaseContoller
     {
+        public VolunteersController(ILogger<VolunteersController> logger)
+            : base(logger)
+        {
+        }
+
         [HttpPost]
         public async Task<ActionResult<Guid>> Create(
             [FromServices] CreateVolunteerHandler handler,
             [FromBody] CreateVolunteerRequest request,
             CancellationToken token)
         {
+            _logger.LogInformation("Create volunteer request");
             var result = await handler.Execute(request, token);
-
             return result.ToResponse<Guid>();
         }
     }
 }
+
+
